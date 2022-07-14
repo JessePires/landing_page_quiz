faf15610dbfe61915e2bbf7faea39ea90a2b00a7
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2022-07-14 16:57:47 -0300

    :bug: Arrumando conflitos.

--- src/components/SliderControlPainel/index.tsx
+++ src/components/SliderControlPainel/index.tsx
@@ -1,29 +1,14 @@
-import React, { useState } from "react";
-import SwipeableViews from "react-swipeable-views";
-import { autoPlay, bindKeyboard } from "react-swipeable-views-utils";
+import React, { useState } from 'react'
+import SwipeableViews from 'react-swipeable-views'
+import { autoPlay, bindKeyboard } from 'react-swipeable-views-utils'
 
 // Icons
-import { IoChevronForwardOutline, IoChevronBackOutline } from "react-icons/io5";
+import { IoChevronForwardOutline, IoChevronBackOutline } from 'react-icons/io5'
 
 // Style
-import {
-  Container,
-  PrevButton,
-  NextButton,
-  ImageWrapper,
-  Image,
-} from "./style";
+import { Container, PrevButton, NextButton, ImageWrapper, Image } from './style'
 
 // Assets
-<<<<<<< HEAD
-import CreateClass from "../../assets/control_painel_screenshots/create_class.png";
-import CreateQuestion from "../../assets/control_painel_screenshots/create_question.png";
-import CreateQuiz from "../../assets/control_painel_screenshots/create_quiz.png";
-import Faq from "../../assets/control_painel_screenshots/faq.png";
-import ListClass from "../../assets/control_painel_screenshots/list_class.png";
-import ManageClass from "../../assets/control_painel_screenshots/manage_class.png";
-import PainelControleQuizUTFPR from "../../assets/control_painel_screenshots/Painel-de-Controle-Quiz-UTFPR.png";
-=======
 import Class from '../../assets/control_painel_screenshots/class.png'
 import ClassDetails from '../../assets/control_painel_screenshots/class_details.png'
 import CreateClass from '../../assets/control_painel_screenshots/create_class.png'
@@ -39,12 +24,11 @@ import SearchImage from '../../assets/control_painel_screenshots/search_image.pn
 import StatisticsQuizQuestion from '../../assets/control_painel_screenshots/statistics_quiz_question.png'
 import StatisticsQuizStudent from '../../assets/control_painel_screenshots/statistics_quiz_student.png'
 import EditQuiz from '../../assets/control_painel_screenshots/edit_quiz.png'
->>>>>>> ac22043ac1ea14f099963b8d49ee7dce37c7ea62
 
-const BindKeyboardSwipeableViews = bindKeyboard(autoPlay(SwipeableViews));
+const BindKeyboardSwipeableViews = bindKeyboard(autoPlay(SwipeableViews))
 
 const SliderControlPainel: React.FC = () => {
-  const [activeImage, setActiveImage] = useState<number>(0);
+  const [activeImage, setActiveImage] = useState<number>(0)
   const images = [
     Login,
     Quizzes,
@@ -60,31 +44,24 @@ const SliderControlPainel: React.FC = () => {
     EditClass,
     CreateClass,
     Faq,
-<<<<<<< HEAD
-    ListClass,
-    ManageClass,
-    PainelControleQuizUTFPR,
-  ];
-=======
     ClassDetails,
   ]
->>>>>>> ac22043ac1ea14f099963b8d49ee7dce37c7ea62
 
   const handleNext = () => {
-    setActiveImage((prevActiveImage) => (prevActiveImage + 1) % images.length);
-  };
+    setActiveImage(prevActiveImage => (prevActiveImage + 1) % images.length)
+  }
 
   const handleBack = () => {
-    setActiveImage((prevActiveImage) => {
-      if (prevActiveImage === 0) return images.length - 1;
+    setActiveImage(prevActiveImage => {
+      if (prevActiveImage === 0) return images.length - 1
 
-      return (prevActiveImage - 1) % images.length;
-    });
-  };
+      return (prevActiveImage - 1) % images.length
+    })
+  }
 
   const handleChangeActiveImage = (step: number) => {
-    setActiveImage(step);
-  };
+    setActiveImage(step)
+  }
 
   return (
     <Container>
@@ -118,7 +95,7 @@ const SliderControlPainel: React.FC = () => {
         </NextButton>
       )}
     </Container>
-  );
-};
+  )
+}
 
-export default SliderControlPainel;
+export default SliderControlPainel
